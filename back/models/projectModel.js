a900443b7aa116d57ad1b56c328d68a0d3568700
ElazbaouiMaryem <ma.elazbaoui@gmail.com>
2020-01-04 16:02:58 +0100

    add swagger

--- back/models/projectModel.js
+++ back/models/projectModel.js
@@ -1,5 +1,50 @@
 const mongoose = require('mongoose');
-
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Project:
+ *        type: object
+ *        required:
+ *          - title
+ *        properties:
+ *          title:
+ *            type: string
+ *          description:
+ *            type: string
+ *          start_date:
+ *            type: string
+ *          end_date:
+ *            type: string
+ *            description: La date de fin doit étre superieure à la date de début.
+ *          create_date:
+ *            type: string
+ *            description: La date de la création du projet.
+ *          users:
+ *            type: array
+ *            properties:
+ *              user_id: string
+ *              role: string
+ *              email: string              
+ *            description: les utilisateurs qui sont inscrits au projet.
+ *          sprints:
+ *            type: array
+ *            properties:
+ *              sprint_id: string
+ *            description: L'ensemble des sprints du projet.
+ *        example:
+ *           description: Projet qui permet de gerer les taches pour les projets entre les utilisateurs,
+ *           start_date: 2019-12-01
+ *           end_date: 2020-01-01
+ *           create_date: 2019-10-26
+ *           title: Gestion des projets
+ *           users:
+ *              user_id: 5da6067b12ed1b1b4c04178c
+ *              role: developpeur
+ *           sprints: 
+ *              sprint_id: 5db4295846817b081cec97d7
+ *      
+ */
 const projectSchema = mongoose.Schema({
 
     title: {
--- back/models/userModel.js
+++ back/models/userModel.js
@@ -1,6 +1,70 @@
 // userModel.js
 var mongoose = require('mongoose');
 // Setup schema
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      User:
+ *        type: object
+ *        required:
+ *          - last_name
+ *          - first_name
+ *          - email
+ *          - password
+ *        properties:
+ *          last_name:
+ *            type: string
+ *          first_name:
+ *            type: string
+ *          password:
+ *            type: string
+ *            descritpion: le mot de passe doit contenir 8 caractères 
+ *          email:
+ *            type: string
+ *            format: email
+ *            description: L'email de l'utilisateur, il doit etre unique.
+ *          create_date:
+ *            type: string
+ *            description: La date de la création de l'utilisateur.
+ *        example:
+ *           last_name: Elazbaoui 
+ *           first_name: maryem
+ *           email: ma.elazbaoui@gmail.com
+ *           password: xxxxxxxx,
+ *           create_date: 2019-01-01
+ *      PostUser:
+ *        example:
+ *           message: created successfuly
+ *           _id: 5e1075b64e1dac4a08464afd
+ *           last_name: Elazbaoui
+ *           first_name: maryem
+ *           email: ma.elazbaoui@gmail.com
+ *           password: $2a$08$VSwpcEoB0XszTwJKTVO6VeiIW.vqDxjH916NqK74B6WbJl.42QU3S
+ *           create_date: 2019-01-01
+ *      getUser:
+ *        example:
+ *           message: retrieved successfuly
+ *           _id: 5e1075b64e1dac4a08464afd
+ *           last_name: Elazbaoui
+ *           first_name: maryem
+ *           email: ma.elazbaoui@gmail.com
+ *           create_date: 2019-01-01
+ *      login:
+ *        example:
+ *           message: loged successfuly
+ *           _id: 5e1075b64e1dac4a08464afd
+ *           last_name: Elazbaoui
+ *           first_name: maryem
+ *           email: ma.elazbaoui@gmail.com
+ *           password: $2a$08$VSwpcEoB0XszTwJKTVO6VeiIW.vqDxjH916NqK74B6WbJl.42QU3S
+ *           create_date: 2019-01-01
+ *      editePassword:
+ *        example:
+ *           old_password: XXXXXXX
+ *           new_password: 123456
+ *           confirmed_password: 123456
+ */
 var userSchema = mongoose.Schema({
     
     email: {
